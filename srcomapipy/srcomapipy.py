390cfd0f8cc0350c3b74c7dbeff99ddf53c8cf5d
Green-Bat <ziad.zaher12@gmail.com>
2024-11-09 01:15:06 +0200

    add readme

--- srcomapipy/srcomapipy.py
+++ srcomapipy/srcomapipy.py
@@ -1,6 +1,6 @@
 import requests
 from typing import Literal, Optional
-from srctypes import *
+from .srctypes import *
 from datetime import date
 
 API_URL = "https://www.speedrun.com/api/v1/"
@@ -335,40 +335,3 @@ class SRC:
         if r.status_code >= 400:
             raise SRCAPIException(r.status_code, uri[len(API_URL) :], r.json())
         return Run(r.json()["data"])
-
-
-def main():
-    from os import getenv
-    from dotenv import load_dotenv
-
-    load_dotenv()
-    api_key = getenv("SRCAPIKEY")
-    api = SRC(api_key=api_key)
-    # me = api.get_users(lookup="GreenBat")[0]
-    # us = api.get_users(lookup="za9c")[0]
-    # game = api.search_game("Batman: Arkham City")[0]
-    # runs: list[Run] = api.get_runs(game, status="new")
-    # print(runs)
-    # for run in runs:
-    #     for p in run.players:
-    #         if p.name == "mahkra":
-    #             success = api.change_run_status(run, status="verified")
-    # print(success)
-    # cat = game.categories["Fastest"]
-    # print(api.generic_get(endpoint="platforms"))
-    # lvl = game.levels["Wayne Manor"]
-    # lbrd = api.get_leaderboard(
-    #     game, cat, lvl, variables=[(cat.variables["Version"], "PC")]
-    # )
-    # print(lbrd, lbrd.top_runs)
-    # for _, cat in game.categories.items():
-    #     print(cat.name)
-    #     for _, v in cat.variables.items():
-    #         print(f"{v} {v.mandatory} {v.obsoletes=} {v.is_subcategory=}")
-    # runs: list[Run] = api.get_runs(game, category=cat.id)
-    # for run in runs:
-    #     print(run)
-
-
-if __name__ == "__main__":
-    main()
--- test.py
+++ test.py
@@ -3,9 +3,38 @@ from srcomapipy.srctypes import *
 
 
 def main():
-    api = SRC()
-    pass
+    from os import getenv
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    api_key = getenv("SRCAPIKEY")
+    api = SRC(api_key=api_key)
+    me = api.get_users(lookup="GreenBat")[0]
+    print(me)
+    # us = api.get_users(lookup="za9c")[0]
+    # game = api.search_game("Batman: Arkham City")[0]
+    # runs: list[Run] = api.get_runs(game, status="new")
+    # print(runs)
+    # for run in runs:
+    #     for p in run.players:
+    #         if p.name == "mahkra":
+    #             success = api.change_run_status(run, status="verified")
+    # print(success)
+    # cat = game.categories["Fastest"]
+    # print(api.generic_get(endpoint="platforms"))
+    # lvl = game.levels["Wayne Manor"]
+    # lbrd = api.get_leaderboard(
+    #     game, cat, lvl, variables=[(cat.variables["Version"], "PC")]
+    # )
+    # print(lbrd, lbrd.top_runs)
+    # for _, cat in game.categories.items():
+    #     print(cat.name)
+    #     for _, v in cat.variables.items():
+    #         print(f"{v} {v.mandatory} {v.obsoletes=} {v.is_subcategory=}")
+    # runs: list[Run] = api.get_runs(game, category=cat.id)
+    # for run in runs:
+    #     print(run)
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
